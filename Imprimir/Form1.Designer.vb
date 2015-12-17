0caf9a40bbcf4f6396ff3cae3b3aeff033d5d463
kvnguevara <kevinguevara_@hotmail.es>
2015-12-17 17:08:55 +0100

    Este proyecto contiene mover imagenes, e imprime

--- Imprimir/Form1.Designer.vb
+++ Imprimir/Form1.Designer.vb
@@ -28,15 +28,19 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.imagen1 = New System.Windows.Forms.PictureBox()
+        Me.imagen2 = New System.Windows.Forms.PictureBox()
+        CType(Me.imagen1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imagen2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(33, 192)
+        Me.Button1.Location = New System.Drawing.Point(111, 160)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(114, 42)
+        Me.Button1.Size = New System.Drawing.Size(100, 42)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Imprimir"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'PrintDocument1
@@ -47,18 +51,18 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(30, 42)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Nombre"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(33, 111)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Apellido"
         '
         'TextBox1
         '
@@ -74,11 +78,33 @@ Partial Class Form1
         Me.TextBox2.Size = New System.Drawing.Size(100, 20)
         Me.TextBox2.TabIndex = 4
         '
+        'imagen1
+        '
+        Me.imagen1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.imagen1.Image = Global.Imprimir.My.Resources.Resources._exit
+        Me.imagen1.Location = New System.Drawing.Point(408, 42)
+        Me.imagen1.Name = "imagen1"
+        Me.imagen1.Size = New System.Drawing.Size(128, 128)
+        Me.imagen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.imagen1.TabIndex = 5
+        Me.imagen1.TabStop = False
+        '
+        'imagen2
+        '
+        Me.imagen2.Location = New System.Drawing.Point(342, 213)
+        Me.imagen2.Name = "imagen2"
+        Me.imagen2.Size = New System.Drawing.Size(264, 123)
+        Me.imagen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.imagen2.TabIndex = 5
+        Me.imagen2.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(391, 390)
+        Me.ClientSize = New System.Drawing.Size(659, 398)
+        Me.Controls.Add(Me.imagen2)
+        Me.Controls.Add(Me.imagen1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
@@ -86,6 +112,8 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.imagen1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imagen2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -97,4 +125,6 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents imagen1 As PictureBox
+    Friend WithEvents imagen2 As PictureBox
 End Class
--- Imprimir/Form1.vb
+++ Imprimir/Form1.vb
@@ -1,10 +1,54 @@
 ﻿Public Class Form1
     Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
         Try
+            'Declaro el stilo de la letra que va a tener nuestra impresion
             Dim prfont As New Font("Arial", 15, FontStyle.Bold)
-
+            'Le indicamos donde tiene que poner la primera letra y el tipo de letra
+            e.Graphics.DrawString(TextBox1.Text & " " & TextBox2.Text, prfont, Brushes.Black, New Point(50, 200))
+            'Le indicamos que no hay mas página
+            e.HasMorePages = False
         Catch ex As Exception
+            MsgBox(ex.Message)
 
         End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaramos un objeto de vista previa
+        Dim vistaprevia As PrintPreviewDialog = New PrintPreviewDialog
+        vistaprevia.Document = PrintDocument1
+        'Definicimos una variable resultado para ver si ha funcionado
+        Dim resultado As DialogResult = vistaprevia.ShowDialog
+        'Compruebi
+        If resultado = DialogResult.OK Then
+            PrintDocument1.Print()
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Le decimos a los pictureBox que las imganes se pueden mover
+        imagen1.AllowDrop = True
+        imagen2.AllowDrop = True
+
+    End Sub
+
+    Private Sub AfterDragDrop(ByVal pic As PictureBox)
+        ' pic.Image = Nothing
+    End Sub
+    'Programamos 
+    Private Sub imagen1_MouseDown(sender As Object, e As MouseEventArgs) Handles imagen1.MouseDown
+        'sender es el que controla el movimiento del raton, si arrastramos o soltamos
+        If sender.DoDragDrop(sender.Image, DragDropEffects.Move) = DragDropEffects.Move Then
+            AfterDragDrop(sender)
+        End If
+    End Sub
+    'cuando soltamos la imagen 
+    Private Sub imagen2_DragEnter(sender As Object, e As DragEventArgs) Handles imagen2.DragEnter
+        e.Effect = DragDropEffects.Move
+    End Sub
+
+    Private Sub imagen2_DragDrop(sender As Object, e As DragEventArgs) Handles imagen2.DragDrop
+        sender.image = DirectCast(e.Data.GetData(DataFormats.Bitmap), Image)
     End Sub
 End Class
